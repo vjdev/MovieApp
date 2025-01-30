6db32b8678a493c53c1b8b71659d5f767b71fc26
vijeesh <vijeeshkumbalath@gmail.com>
2025-01-30 14:36:52 +0530

    Initial Commit

---
+++ MovieApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieApp
+//
+//  Created by vijeesh k on 30/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
